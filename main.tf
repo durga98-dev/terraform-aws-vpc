c99163b0f9b7a85502c1da44bbf8a981956397e4
Kapavarapu <durgadevi.kapavarapu@accenture.com>
2025-07-15 23:03:03 +0530

    commiting terraform aws vpc module

--- main.tf
+++ main.tf
@@ -143,14 +143,14 @@ resource "aws_route" "public" {
 resource "aws_route" "private" {
   route_table_id            = aws_route_table.private.id
   destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_nat_gateway.example.id
+  nat_gateway_id = aws_nat_gateway.example.id
 }
 
 #create route for database
 resource "aws_route" "database" {
   route_table_id            = aws_route_table.database.id
   destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_nat_gateway.example.id
+  nat_gateway_id = aws_nat_gateway.example.id
 }
 
 resource "aws_route_table_association" "public" {
