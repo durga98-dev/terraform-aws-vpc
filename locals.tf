48b543e083fc1d8fc41dc2d04f087462888fa978
Kapavarapu <durgadevi.kapavarapu@accenture.com>
2025-11-19 22:57:54 +0530

    committing terraform-aws-vpc

--- locals.tf
+++ locals.tf
@@ -1,6 +1,4 @@
-locals{
-    resource_name = "${var.project_name}-${var.environment}"
-    azs_names = slice(data.aws_availability_zones.available.names, 0 , 2)
-    default_vpc_id = data.aws_vpc.default.id
-    default_vpc_cidr = data.aws_vpc.default.cidr_block
+locals {
+  resource_name = "${var.project_name}-${var.environment}"
+  az_names = slice(data.aws_availability_zones.available, 0 ,2)
 }
--- variables.tf
+++ variables.tf
@@ -7,21 +7,20 @@ variable "environment" {
 }
 
 variable "vpc_cidr"{
-
+  
 }
 
-variable "enable_dns_hostnames"{
-    default = true
+variable "enable_dnshostnames"{
+  default = true
 }
 
-#Mandatory
-variable "common_tags"{
-    type = map
-    default = {}
+# Mandatory
+variable "common_tags" {
+  
 }
 
+# Flexibility for the user to add more tags - optional.
 variable "vpc_tags" {
-  type = map
   default = {}
 }
 
@@ -29,64 +28,39 @@ variable "igw_tags" {
   default = {}
 }
 
-variable "public_subnet_tags" {
-  default = {}
-}
-
 variable "public_subnet_cidrs" {
   type = list
   validation {
-    condition = length(var.public_subnet_cidrs)==2
-    error_message = "Please provide 2 valid public subnet CIDR"
+    condition = length(var.public_subnet_cidrs) == 2
+    error_message = "Must provide two valid public subnet CIDR"
   }
+}
+
+variable "private_subnet_cidrs" {
+  type = list
+  validation {
+    condition = length(var.private_subnet_cidrs) == 2
+    error_message = "Must provide two valid private subnet CIDR"
+  }
+}
+
+variable "db_subnet_cidrs" {
+  type = list
+  validation {
+    condition = length(var.db_subnet_cidrs) == 2
+    error_message = "Must provide two valid db subnet CIDR"
+  }
+}
+
+variable "public_subnet_tags" {
+  default = {}
 }
 
 variable "private_subnet_tags" {
   default = {}
 }
 
-variable "private_subnet_cidrs" {
-  type = list
-  validation {
-    condition = length(var.private_subnet_cidrs)==2
-    error_message = "Please provide 2 valid private subnet CIDR"
-  }
-}
-
-variable "database_subnet_tags" {
+variable "db_subnet_tags" {
   default = {}
 }
-
-variable "database_subnet_cidrs" {
-  type = list
-  validation {
-    condition = length(var.database_subnet_cidrs)==2
-    error_message = "Please provide 2 valid private subnet CIDR"
-  }
-}
-
-variable "natgateway_tags" {
-  default = {}
-}
-
-variable "public_route_tags" {
-  default = {}
-}
-
-variable "private_route_tags" {
-  default = {}
-}
-
-variable "database_route_tags" {
-  default = {}
-}
-
-variable "is_peering_required" {
-  default = false
-}
-
-variable "vpc_peering_tags" {
-  default = {}
-}
-
 
