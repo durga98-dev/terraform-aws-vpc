832108d68bbd80c4569f980bfa874fc95b32441e
Kapavarapu <durgadevi.kapavarapu@accenture.com>
2025-11-19 23:35:09 +0530

    committing terraform-aws-vpc

--- main.tf
+++ main.tf
@@ -139,3 +139,10 @@ resource "aws_route" "private" {
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_nat_gateway.example.id
 }
+
+resource "aws_route" "db" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_nat_gateway.example.id
+}
+
